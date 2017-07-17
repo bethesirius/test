60367af1cf8a82e312cdf9f499f8cf59d46fb38a
Seongjoon Chu <hs4393@gmail.com>
2017-07-17 11:24:57 +0900

    Update Vulnerable.java

--- Vulnerable/src/main/java/Vulnerable.java
+++ Vulnerable/src/main/java/Vulnerable.java
@@ -4,6 +4,11 @@ class Vulnerable {
 
 String generateSecretToken() {
     Random r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
     return Long.toHexString(r.nextLong());
 }
 }
