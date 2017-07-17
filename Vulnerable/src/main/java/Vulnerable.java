c002360bc527ccac25c2cec2c69d4fd2d645b907
Seongjoon Chu <hs4393@gmail.com>
2017-07-17 11:32:58 +0900

    Update Vulnerable.java

--- Vulnerable/src/main/java/Vulnerable.java
+++ Vulnerable/src/main/java/Vulnerable.java
@@ -1,6 +1,34 @@
 import java.util.Random;
 
 class Vulnerable {
+    String generateSecretToken1() {
+    Random r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    return Long.toHexString(r.nextLong());
+}
+    String generateSecretToken2() {
+    Random r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    return Long.toHexString(r.nextLong());
+}
+    
+    String generateSecretToken3() {
+    Random r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    r = new Random();
+    return Long.toHexString(r.nextLong());
+}
 
 String generateSecretToken() {
     Random r = new Random();
